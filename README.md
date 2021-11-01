805d6b7b0c777134697849da42880a072ac086ea
Jefferson Amstutz <jamstutz@nvidia.com>
2021-11-01 14:34:35 -0500

    initial upload of the v1.0-provisional specification

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ANARI-Registry
-ANARI Specification Registry
+
+The ANARI-Registry repository contains the ANARI 1.0-Provisional Specification.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/ANARI/. Commits to the main branch of this
+repository will be reflected there.
+
+Interesting files in this repository include:
+
+* `index.php` - toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* `specs/` - ANARI specification documents.
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$static_title = 'Khronos ANARI Registry';
+
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<p> The ANARI registry contains the specification of the ANARI 3D visualization interface API. </p>
+
+<h2> <a name="specs"></a> <b>ANARI Specification</b> </h2>
+
+<p> The current version of ANARI is 1.0-provisional: </p>
+
+<ul>
+<li> ANARI 1.0 Provisional Specification in
+     <a href="https://github.com/KhronosGroup/ANARI/tree/main/specification/1.0-provisional">
+     Markdown</a> (rendered by GitHub). </li>
+</ul>
+
+<p> Khronos welcomes comments and issue reports. </p>
+
+<p> To provide feedback <strong>on the ANARI Specification</strong>, file an
+    issue in the <a href="https://github.com/KhronosGroup/ANARI-Docs/issues/new">
+    KhronosGroup/ANARI-Docs</a> GitHub project. </p>
+
+<p> To provide feedback <strong>on the ANARI SDK</strong>, file an
+    issue in the <a href="https://github.com/KhronosGroup/ANARI-SDK/issues/new">
+    KhronosGroup/ANARI-SDK</a> GitHub project. </p>
+
+<p> To provide feedback <strong>on the ANARI registry itself</strong> (such
+    as reporting missing content, bad links, etc.), file an issue in the <a
+    href="https://github.com/KhronosGroup/ANARI-Registry/issues/new">
+    ANARI-Registry</a> GitHub project. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
